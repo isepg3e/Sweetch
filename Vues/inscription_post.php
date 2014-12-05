1ff16f6116da03662f375481a2c7e7859374891b
bebec <jordanhirbec@yahoo.fr>
2014-12-05 12:27:41 +0100

    modif sympa

--- Vues/inscription_post.php
+++ Vues/inscription_post.php
@@ -1,4 +1,13 @@
 <?php
+$nom=$_POST['nom'];
+$prenom=$_POST['prenom'];
+$pseudo=$_POST['pseudo'];
+$mdp=$_POST['mdp'];
+$con=$_POST['confirmation'];
+$email = $_POST['email'];
+$point = strpos($email,".");
+$aroba = strpos($email,"@");
+$checkbox=$_POST['checkbox'];
 // D'abord, je me connecte à la base de données!!!
 try
 {
@@ -11,22 +20,32 @@ catch(Exception $e)
 die('Erreur : '.$e->getMessage());
 }
 
-
-if($motdepasse == $confirmation)
+if($mdp != $con)
 {
-// Insertion du formulaire dans la BDD à l'aide d'une requête préparée
-$req = $bdd->prepare('INSERT INTO membre(nom, prenom,age) VALUES(?,?,?)');
-$dateFormated = split('/', $_POST['age']);
-$date = $dateFormated[2].'-'.$dateFormated[1].'-'.$dateFormated[0];
-$req->execute(array($_POST['nom'], $_POST['prenom'], $date));
-
-
-// Redirection du visiteur vers la page du minichat
-
+	echo 'Les deux mots de passe que vous avez rentrés ne correspondent pas';
 }
+elseif(empty($checkbox))
+{
+	echo 'coche les CGU';
+}
+elseif(empty($nom) OR empty($prenom) OR empty($email) OR empty($mdp) OR empty($pseudo) OR empty($con) OR empty($_POST['age']))
+{
+	echo 'remplistoutes les cases';
+}
+if($point=='' OR $aroba=='')
+{
+echo "Votre email n'est pas valide";
+}
+
 else
 {
-echo 'Les deux mots de passe que vous avez rentrés ne correspondent pas…';
+	// Insertion du formulaire dans la BDD à l'aide d'une requête préparée
+
+$req = $bdd->prepare('INSERT INTO membre(nom, prenom,age,email,mdp,CGU,pseudo) VALUES(?,?,?,?,?,?,?)');
+$dateFormated = split('/', $_POST['age']);
+$date = $dateFormated[2].'-'.$dateFormated[1].'-'.$dateFormated[0];
+$req->execute(array($nom, $prenom, $date, $email, $mdp, $checkbox, $pseudo));
 }
 
 ?>
+
